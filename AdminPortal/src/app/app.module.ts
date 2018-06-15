aef385a7fd1325e598a75b4ce82911653cf37ea0
Abhiram Mandavilli <phantom@cometnet-10-21-9-194.utdallas.edu>
2018-06-15 13:16:21 -0500

    Setup admin portal

---
+++ AdminPortal/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {routing} from './app.routing';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
