b31de5a7469e8ebabdcca5d5c2fa91c49c4b245a
Abhiram Mandavilli <phantom@cometnet-10-21-62-55.utdallas.edu>
2018-06-16 19:47:14 -0500

    Admin Portal Login Service with Angular

--- AdminPortal/src/app/app.module.ts
+++ AdminPortal/src/app/app.module.ts
@@ -7,12 +7,15 @@ import { NavbarComponent } from './navbar/navbar.component';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {routing} from './app.routing';
+import { LoginComponent } from './login/login.component';
+import {LoginService} from './login.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +23,7 @@ import {routing} from './app.routing';
     HttpModule,
     routing
   ],
-  providers: [],
+  providers: [LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
